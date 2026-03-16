fea7b901bd03a3355fd35625a02a9cbcbb7ebf78
lrsiguan <lrisguan@outlook.com>
2026-03-16 20:20:10 +0800

    update: remove duplicate whitespace

--- src/SymbolTable.cc
+++ src/SymbolTable.cc
@@ -2,7 +2,7 @@
  * ccc
  * Copyright (c) 2026 lrisguan <lrisguan@outlook.com>
  * 
- * This program is released under the terms of the  GNU General Public License version 2(GPLv2).
+ * This program is released under the terms of the GNU General Public License version 2(GPLv2).
  * See https://opensource.org/licenses/GPL-2.0 for more information.
  * 
  * Project homepage: https://github.com/lrisguan/ccc
